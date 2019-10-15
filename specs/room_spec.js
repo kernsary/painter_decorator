61c0b414b6f4df9b8045693e4dfce6a7980f29ea
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-15 15:34:52 +0100

    initial commit

---
+++ specs/room_spec.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const Room = require("../room.js");
+
+describe("Room", function(){
+
+  let room;
+
+  beforeEach(function(){
+    room = new Room(30);
+  });
+
+  it("should have an area", function(){
+    const actual = room.area;
+    assert.strictEqual(actual, 30);
+  });
+
+})
